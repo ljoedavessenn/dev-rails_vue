f3dbbff48f57a9e333edf0479cc8756596fb5216
Davessenn Louis-Jeune <mlouisjeune@pih.org>
2022-05-01 15:10:55 -0400

    test all

--- blog/config/environments/development.rb
+++ blog/config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "ec2-3-141-21-187.us-east-2.compute.amazonaws.com"
+
 end
